9965c22557958cf4cb0dce5dc0afcc6712deaca4
Pramish Luitel <luitelpramish7@gmail.com>
2019-08-25 21:14:21 +1000

    home page ui partially completed

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './HomeComponent/NavBarComponent/NavBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <br />
+        <NavBar />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ client/src/HomeComponent/NavBarComponent/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left'
+  }
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          ></IconButton>
+          <Typography variant='h6' className={classes.title}>
+            PHOTOBOOTH
+          </Typography>
+          <Button
+            aria-controls='customized-menu'
+            aria-haspopup='true'
+            variant='contained'
+            color='primary'
+            // onClick={handleClick}
+          >
+            SignIn/SignUp
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
