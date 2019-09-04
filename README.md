cf8a4c8e5ee3d35e88f2ec8497fc0ab2496c7ea6
Pramish Luitel <luitelpramish7@gmail.com>
2019-09-05 07:32:33 +1000

    week 7 lab is completed

---
+++ README.md
@@ -0,0 +1,8 @@
+To start the project please add the following:
+
+-
+module.exports = {
+MONGOURI:
+your MONGOURI,
+secretOrKey: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('./config/db');
+const mongo = require('./config/keys').MONGOURI;
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+const userRouter = require('./routes/api/users.routes');
+const passport = require('passport');
+// require('./config/passport')(passport);
+
+//Middleware Bodyparser
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.use(bodyParser.json());
+
+//using the passport middleware
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+//Configure the passport
+
+//Routes for the users
+app.use('/api/users', userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is connected to port ${port}`);
+});
