b1c045bc31f502ded10361ab4c29a6f1bee4d957
Pramish Luitel <luitelpramish7@gmail.com>
2019-09-14 20:01:44 +1000

    now defining the logic for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,12 +6,22 @@ import { uploadImage } from '../../actions/authActions';
 import classnames from 'classnames';
 
 class Dashboard extends Component {
-  render() {
-    // const { errors } = this.state;
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: ''
+    };
+  }
 
+  render() {
     return (
       <div>
         <h1>Hello I am a dashboard</h1>
+        {/* 
+          I have to add the logic i.e. if the user logged in then login button will change to logout and register button will
+          change into edit profile
+        */}
       </div>
     );
   }
