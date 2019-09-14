d00fd1c1f208f8bed02611eff1511251e0a2c5fe
Pramish Luitel <luitelpramish7@gmail.com>
2019-09-14 19:56:24 +1000

    editting the homepage and dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,84 +6,15 @@ import { uploadImage } from '../../actions/authActions';
 import classnames from 'classnames';
 
 class Dashboard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      image: '',
-      errors: {}
-    };
-  }
-
-  componentDidMount() {
-    // If logged in and user navigates to Register page, should redirect them to dashboard
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
-    }
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-    const image = {
-      image: this.state.image
-    };
-    this.props.uploadImage(image, this.props.history);
-  };
-
   render() {
     // const { errors } = this.state;
 
     return (
-      <div style={{ height: '75vh' }} className='container valign-wrapper'>
-        <div className='row'>
-          <div className='landing-copy col s12 center-align'>
-            {/* <b>Hey there,</b> {user.name.split(" ")[0]} */}
-            <p className='flow-text grey-text text-darken-1'>
-              <span style={{ fontFamily: 'monospace' }}>Upload</span> a Image 📸
-            </p>
-
-            <form onSubmit={this.onSubmit}>
-              <div className='input-field col s12'>
-                <input id='file' type='file' />
-              </div>
-
-              <button
-                style={{
-                  width: '150px',
-                  borderRadius: '3px',
-                  letterSpacing: '1.5px',
-                  marginTop: '1rem'
-                }}
-                type='submit'
-                className='btn btn-large waves-effect waves-light hoverable blue accent-3'
-              >
-                Upload
-              </button>
-            </form>
-          </div>
-        </div>
+      <div>
+        <h1>Hello I am a dashboard</h1>
       </div>
     );
   }
 }
 
-Dashboard.propTypes = {
-  uploadImage: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
-
-export default connect(
-  mapStateToProps,
-  { uploadImage }
-)(withRouter(Dashboard));
+export default Dashboard;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,21 +4,18 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <div className='navbar-fixed'>
-        <nav className='z-depth-0'>
-          <div className='nav-wrapper white'>
-            <Link
-              to='/'
-              style={{
-                fontFamily: 'monospace'
-              }}
-              className='col s5 brand-logo center black-text'
-            >
-              PhotoBooth
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <nav>
+        <div className='nav-wrapper'>
+          <ul id='nav-mobile' class='right hide-on-med-and-down'>
+            <li>
+              <a href='/login'>Login</a>
+            </li>
+            <li>
+              <a href='/register'>Register</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
