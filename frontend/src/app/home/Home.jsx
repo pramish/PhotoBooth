a865a613cc0fe1976f58b05dc87d47cc7d3804a4
Rohit Gurung <12695856@student.uts.edu.au>
2019-09-30 10:17:30 +1000

    Minor Changes

--- frontend/src/app/home/Home.jsx
+++ frontend/src/app/home/Home.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Navbar from "../common/Navbar";
 import SearchBar from "../common/SearchBar";
 import EachFeed from "./EachFeed";
+import SignIn from "./SignIn";
 
 const Home = () => {
+  const [state, setState] = useState(false);
+  const change = () => {
+    setState(!state);
+  };
   return (
     <Container>
-      <Navbar />
+      <Navbar onClickHandler={change} />
+      <SignIn boo={state} />
       <SearchBar />
       <div className="feedContainer">
         <EachFeed />
