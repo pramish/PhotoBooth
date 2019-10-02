2bc6382535a4410c7572202ce118bb3d92ad9821
Pramish Luitel <luitelpramish7@gmail.com>
2019-10-03 00:19:04 +1000

    CRUD for users is now complete. Only left is the Update the user other than that all is working perfectly.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const router = require("express").Router();
-const userRouter = require("./api/users/users.router");
-const feedRouter = require("./api/feeds/feeds.router");
-const errorHandler = require("../services/errorHandler");
+const router = require('express').Router();
+const userRouter = require('./api/users/users.router');
+const feedRouter = require('./api/feeds/feeds.router');
+const errorHandler = require('../services/errorHandler');
 
-router.use("/users", userRouter);
-router.use("/feeds", feedRouter);
+router.use('/users', userRouter);
+router.use('/feeds', feedRouter);
 router.use(errorHandler);
 
 module.exports = router;
